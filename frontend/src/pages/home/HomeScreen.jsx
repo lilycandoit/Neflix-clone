8241dff6953e801bc10964528e7d217760b613a6
lily <hueduong288@gmail.com>
2024-11-13 17:33:34 +1100

    create Navbar and Homescreen with overlay on Hero img

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -1,14 +1,54 @@
 import React from 'react';
-import { useAuthStore } from '../../store/authUser';
+import Navbar from '../../components/Navbar';
+import { Link } from 'react-router-dom';
+import { Info, Play } from 'lucide-react';
 
 const HomeScreen = () => {
-  const { logout } = useAuthStore();
-
   return (
-    <div>
-      HomeScreen
-      <button onClick={logout} className='bg-red-400'> Logout</button>
-    </div>
+    <>
+      <div className="relative h-screen text-white">
+        <Navbar />
+
+        <img
+          src="/extraction.jpg"
+          alt="Hero img"
+          className="absolute top-0 left-0 w-full h-full object-cover -z-50"
+        />
+
+        <div
+          className="absolute top-0 left-0 w-full h-full bg-black/50 -z-50"
+          aria-hidden="true"
+        />
+
+        <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center px-8 md:px-16 lg:px-32 ">
+          <div className="bg-gradient-to-b from-black via-transparent to-transparent absolute w-full h-full top-0 left-0 -z-10"></div>
+
+          <div className="max-w-2xl">
+            <h1 className="mt-4 text-6xl font-extrabold text-balance">
+              Extraction
+            </h1>
+            <p className="mt-2 text-lg"> 2014 | 18+</p>
+            <p className="mt-4 text-lg"> will be fetched from backend</p>
+          </div>
+
+          <div className="flex mt-8">
+            <Link
+              to={'/watch/123'}
+              className="bg-white hover:bg-white/60 text-black font-bold py-2 px-4 rounded mr-4 flex items-center justify-center"
+            >
+              <Play className="size-6 mr-2 fill-black" /> Play
+            </Link>
+
+            <Link
+              to={'/watch/123'}
+              className="bg-gray-500/70 hover:bg-gray-500 text-white py-2 px-4 rounded mr-4 flex items-center justify-center"
+            >
+              <Info className="size-6 mr-2" /> More Info
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
